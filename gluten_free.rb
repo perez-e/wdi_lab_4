c770be153b70b794e46fbf087be5fab7b21dcb90
Eric Perez <ericricardoperez@gmail.com>
2014-01-09 17:50:36 -0800

    debugged the Person class

--- gluten_free.rb
+++ gluten_free.rb
@@ -16,10 +16,33 @@
 # If a food array contains a known allergy reject the food.
  
 class Person
+	attr_reader :name
+	def initialize(name, allergy)
+		@name = name
+		@allergy = allergy
+		@stomach = []
+	end
+
+	def eat(food)
+		@stomach += food
+		@stomach.each do |item|
+			if item == @allergy
+					raise AllergyError.new("Oh, no! I'm allergic to #{item}!!")
+			end
+		end
+		rescue 
+			puts "Spit the food out."
+			@stomach = []
+	end
+
 end
  
 # Create a Person named Chris. Chris is allergic to gluten.
+chris = Person.new("Chris", "gluten")
+
 # Create a Person named Beth. Beth is allergic to scallops.
+beth = Person.new("Beth", "scallops")
+
 # Feed them the following foods
  
 pizza = ["cheese", "gluten", "tomatoes"]
@@ -28,7 +51,10 @@ water = ["h", "h", "o"]
  
 # When a person attempts to eat a food they are allergic to, raise a custom exception
 # For example:  AllergyError
- 
+
+class AllergyError < StandardError
+end
+
  
 # Bonus: When a person attempts to eat a food they are allergic to,
 #        ... remove ALL the food from the person's stomach before raising the exception
